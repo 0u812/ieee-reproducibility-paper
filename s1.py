73f31addb8e3d22f641db0cf970ee504804b9a9e
jkmedley <0u812@users.noreply.github.com>
2016-06-02 11:13:28 -0700

    add installation info for mac tel

--- s1.py
+++ s1.py
@@ -2,6 +2,7 @@
 # Author: J Kyle Medley
 # Python 2.7.10, Tellurium 1.3.3 for Mac OS X 10.10
 # Download: https://sourceforge.net/projects/pytellurium/files/Tellurium-1.3/1.3.3/Tellurium-1.3.3-Spyder-2.3.8-OSX.dmg/download
+# Installation: Double-click Tellurium-1.3.3-Spyder-2.3.8-OSX.dmg and open "Spyder-Py2"
 # roadrunner version:
 # 1.4.5; Compiler: clang "6.0 (clang-600.0.56)", C++ version: 199711; JIT Compiler: LLVM-3.5; Date: May 12 2016, 19:12:23; LibSBML Version: 5.12.0
 
